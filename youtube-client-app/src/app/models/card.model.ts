f507cc2646e895f5c0a813f30d3215dc1238926d
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-10-23 21:41:40 +0300

    feat: add card model

---
+++ youtube-client-app/src/app/models/card.model.ts
@@ -0,0 +1,56 @@
+export interface CardItems {
+  TODO: string;
+
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: Card[];
+}
+
+export interface Card {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: Thumbnails;
+      medium: Thumbnails;
+      high: Thumbnails;
+      standard: Thumbnails;
+      maxres: Thumbnails;
+    };
+    channelTitle: string;
+    tags: Array<string>;
+    categoryId: string;
+    liveBroadcastContent: string;
+    localized: {
+      title: string;
+      description: string;
+    };
+    defaultAudioLanguage: string;
+  };
+  statistics: Statistics;
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+interface Thumbnails {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
