8b0c89934bd77cb818da8cb45fdc740606065b30
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-11-06 04:49:02 +0300

    feat: add not-found page

--- youtube-client-app/src/app/router/app-router.module.ts
+++ youtube-client-app/src/app/router/app-router.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { YouTubePageComponent } from '../youtube/pages/youtube/youtube-page.component';
+import { NotfoundPageComponent } from '../youtube/pages/notfound-page/notfound-page.component';
 
 const routes: Routes = [
   {
     path: 'youtube',
-    component: YouTubePageComponent,
+    // component: YouTubePageComponent,
+    component: NotfoundPageComponent,
   },
   { path: '', redirectTo: 'youtube', pathMatch: 'full' },
 ];
--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -6,11 +6,17 @@ import { YouTubePageComponent } from './pages/youtube/youtube-page.component';
 import { CardColorDirective } from './directives/card-color/card-color.directive';
 import { SharedModule } from '../shared/shared.module';
 import { SearchDataService } from './services/search-data/search-data.service';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 
 @NgModule({
-  declarations: [CardComponent, YouTubePageComponent, CardColorDirective],
+  declarations: [
+    CardComponent,
+    YouTubePageComponent,
+    CardColorDirective,
+    NotfoundPageComponent,
+  ],
   imports: [SharedModule, CommonModule],
   providers: [SearchDataService],
-  exports: [YouTubePageComponent],
+  exports: [YouTubePageComponent, NotfoundPageComponent],
 })
 export class YoutubeModule {}
