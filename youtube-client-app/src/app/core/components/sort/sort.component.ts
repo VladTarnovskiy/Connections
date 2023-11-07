41c901b95a5a2f45893a8641ea9f0c68ad14f1f1
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-11-08 02:26:39 +0300

    refactor: add kind of sort to enum

--- youtube-client-app/src/app/core/components/sort/sort.component.ts
+++ youtube-client-app/src/app/core/components/sort/sort.component.ts
@@ -1,34 +1,39 @@
 import { Component } from '@angular/core';
 import { SearchDataService } from 'src/app/youtube/services/search-data/search-data.service';
 
+enum Position {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
 @Component({
   selector: 'app-sort',
   templateUrl: './sort.component.html',
   styleUrls: ['./sort.component.scss'],
 })
 export class SortComponent {
-  sortByDateDir = 'desc';
+  sortByDateDir = Position.DESC;
 
-  sortByViewDir = 'desc';
+  sortByViewDir = Position.DESC;
 
   filterString = '';
 
   constructor(private dataService: SearchDataService) {}
 
   sortByDate() {
-    if (this.sortByDateDir === 'desc') {
-      this.sortByDateDir = 'asc';
+    if (this.sortByDateDir === Position.DESC) {
+      this.sortByDateDir = Position.ASC;
     } else {
-      this.sortByDateDir = 'desc';
+      this.sortByDateDir = Position.DESC;
     }
     this.dataService.sort({ sortBy: 'date', direction: this.sortByDateDir });
   }
 
   sortByView() {
-    if (this.sortByViewDir === 'desc') {
-      this.sortByViewDir = 'asc';
+    if (this.sortByViewDir === Position.DESC) {
+      this.sortByViewDir = Position.ASC;
     } else {
-      this.sortByViewDir = 'desc';
+      this.sortByViewDir = Position.DESC;
     }
 
     this.dataService.sort({ sortBy: 'view', direction: this.sortByViewDir });
