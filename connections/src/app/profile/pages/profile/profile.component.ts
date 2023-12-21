91bc1b7590d04322fa5af46605c2a2219029d683
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-12-22 01:21:54 +0300

    refactor: save button disabled

--- connections/src/app/profile/pages/profile/profile.component.ts
+++ connections/src/app/profile/pages/profile/profile.component.ts
@@ -8,10 +8,8 @@ import {
   selectProfileLoading,
 } from 'src/app/store/profile/selectors/profile.selectors';
 import * as ProfileActions from 'src/app/store/profile/actions/profile.action';
-import * as AuthActions from 'src/app/store/auth/actions/auth.action';
 import { FormControl, Validators } from '@angular/forms';
 import { ValidateName } from 'src/app/auth/pages/register/validators.ts/name';
-import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -21,6 +19,7 @@ import { AuthService } from 'src/app/auth/services/auth.service';
 export class ProfileComponent implements OnInit, OnDestroy {
   profileData$: Observable<IProfile | null> =
     this.store.select(selectProfileData);
+  profileData!: IProfile | null;
   isLoading$: Observable<boolean> = this.store.select(selectProfileLoading);
   edit$: Observable<boolean> = this.store.select(selectProfileEdit);
   edit = false;
@@ -32,7 +31,7 @@ export class ProfileComponent implements OnInit, OnDestroy {
     ValidateName(),
   ]);
 
-  constructor(private store: Store, private authService: AuthService) {}
+  constructor(private store: Store) {}
 
   editName() {
     this.store.dispatch(ProfileActions.ChangeEditProfile({ edit: true }));
@@ -40,6 +39,9 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   cancelEdit() {
     this.store.dispatch(ProfileActions.ChangeEditProfile({ edit: false }));
+    if (this.profileData) {
+      this.name.setValue(this.profileData.name);
+    }
   }
 
   saveName(name: string) {
@@ -63,7 +65,17 @@ export class ProfileComponent implements OnInit, OnDestroy {
       this.edit = value;
     });
 
+    const secondChildSubscription = this.profileData$.subscribe(
+      (profileData) => {
+        this.profileData = profileData;
+        if (this.profileData) {
+          this.name.setValue(this.profileData.name);
+        }
+      }
+    );
+
     this.subscription.add(childSubscription);
+    this.subscription.add(secondChildSubscription);
   }
 
   ngOnDestroy(): void {
